811a0255ad4e9e62cc2aaac48b3a98d58c33380c
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2018-09-19 02:07:45 +0100

    Add comment

--- filter-risk.py
+++ filter-risk.py
@@ -7,6 +7,7 @@ import xml.etree.ElementTree as ET
 plugin_paths = glob.glob("nessus-samples/*.nasl") + glob.glob("nessus-samples/*.nbin")
 
 for path in plugin_paths:
+    # it should be more efficient to call 'nasl' with multiple files
     xml = subprocess.check_output(["/opt/nessus/bin/nasl", "-VVVVV", path])
 
     root = ET.fromstring(xml)
