6f9409767c81e6ffd70fa4e3d6045bc93d8d3f19
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2018-09-19 01:16:10 +0100

    Get XML for each file

--- filter-risk.py
+++ filter-risk.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
 import glob
+import subprocess
 
 plugin_paths = glob.glob("nessus-samples/*.nasl") + glob.glob("nessus-samples/*.nbin")
+
+for path in plugin_paths:
+    xml = subprocess.check_output(["/opt/nessus/bin/nasl", "-VVVVV", path])
+
