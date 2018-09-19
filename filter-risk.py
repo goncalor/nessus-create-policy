bb4001c251074410ffc1f0e1a2cb25437b6acb0a
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2018-09-19 01:56:34 +0100

    Start parsing XML

--- filter-risk.py
+++ filter-risk.py
@@ -2,9 +2,12 @@
 
 import glob
 import subprocess
+import xml.etree.ElementTree as ET
 
 plugin_paths = glob.glob("nessus-samples/*.nasl") + glob.glob("nessus-samples/*.nbin")
 
 for path in plugin_paths:
     xml = subprocess.check_output(["/opt/nessus/bin/nasl", "-VVVVV", path])
 
+    root = ET.fromstring(xml)
+    risk_factor = root.find('attributes/attribute[name="risk_factor"]/value').text
