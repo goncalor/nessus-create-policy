1810b29ef3d4665c7ad3a260b41fb5d47b40bd55
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2018-10-07 13:46:49 +0100

    Prefer risk factor to CVSSv3; and CVSSv3 to CVSSv2

--- filter-plugins.py
+++ filter-plugins.py
@@ -9,16 +9,13 @@ def calc_severity(info):
     cvss3 = info['cvss3']
     risk = info['risk_factor']
 
-    if cvss2:
-        if cvss2 >= 9.0:
-            return 'Critical'
-        elif cvss2 >= 7.0:
-            return 'High'
-        elif cvss2 >= 4.0:
-            return 'Medium'
-        else:
-            return 'Low'
+    if risk:
+        if risk == 'None':
+            return 'Info'
+        return risk
 
+    # As of October 2018 all scripts that have a CVSS3 vector also have CVSS2.
+    # Therefore, CVSS3 needs to be tested first to have any effect at all
     if cvss3:
         if cvss3 >= 9.0:
             return 'Critical'
@@ -29,9 +26,14 @@ def calc_severity(info):
         else:
             return 'Low'
 
-    if risk:
-        if risk == 'None':
-            return 'Info'
-        return risk
+    if cvss2:
+        if cvss2 >= 9.0:
+            return 'Critical'
+        elif cvss2 >= 7.0:
+            return 'High'
+        elif cvss2 >= 4.0:
+            return 'Medium'
+        else:
+            return 'Low'
 
     return None
